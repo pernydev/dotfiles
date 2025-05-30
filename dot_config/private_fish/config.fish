3aed1f13553527b27d54d0e621a52d9c282f73a1
Perny <git@perny.dev>
2025-05-30 11:28:06 +0300

    fish

---
+++ dot_config/private_fish/config.fish
@@ -0,0 +1,11 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+alias rl "source ~/.config/fish/config.fish"
+alias nf fastfetch
+
+set fish_greeting ""
+
+echo ""
+nf
